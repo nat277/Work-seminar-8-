af3f5ebf0cc2f10a9904e9a3615f2ed930786276
«nat» <nat27.kozlowa@yandex.ru>
2023-05-26 08:51:13 +0400

    сделала

--- Team_work/controller.py
+++ Team_work/controller.py
@@ -4,16 +4,20 @@ import  model, view
 def start():
     view.greetings()
     while True:
+        
         view.menu() 
-        activ = input('Введите команду: ') 
+        activ = input("Выберите действие от 1 до 6,где 1. вывести все контакты: 2. поиск контактов: 3. добавить контакт: 4. изменить контакт: 5. удалить: 6. выход: ")
+        print()
+
+        activ = input('Действие: ') 
         if activ == '1':
-            pass #все контакты
+           view.display_contacts(data) #все контакты
         elif activ == '2':
             data = input('Введите ФИО: ')
             model.search_contacts(data) #поиск контактов
         elif activ== '3':
             data = input('Введите ФИО: ')
-            add_contacts(data) #добавить контакт
+            view.add_contact(data) #добавить контакт
         elif activ == '4': 
             data = input('какой контакт хотите изменить ?: ')
             model.change_contacts(data) # изменить контакт
